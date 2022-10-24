dd3f4d09e0c6d5591f46650a35ed577c0f3f7fca
Your Name <you@example.com>
2022-10-23 22:16:19 -0600

    Made adjustments to Models fields for classes + Started adding requests to Views.

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from authentication.models import User
-
+from django.db import models;
+from authentication.models import User;
+from django.db.models.fields import BLANK_CHOICE_DASH;
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -9,10 +9,21 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True,editable=False)
     brand = models.CharField(max_length=255)     
     price = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True
+    createdAt = models.DateTimeField(auto_now_add=True)
     description = models.TextField(max_length=255)
 
     def __str__(self):
         return self.name +" | "+self.brand +" | " + str(self.price)
+        
 
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+    # order  = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+    name = models.CharField(max_length=200,null=True,blank=True)
+    # qty = models.IntegerField(null=True,blank=True,default=0)
+    price = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    image = models.CharField(max_length=200,null=True,blank=True)
+    _id =  models.AutoField(primary_key=True,editable=False)
 
+    def __str__(self):
+        return str(self.name)
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from itertools import product
+from math import prod
+from django.shortcuts import render;
+from django.shortcuts import get_object_or_404;
+from rest_framework import status;
+from rest_framework.response import Response;
+from rest_framework.permissions import IsAuthenticated, AllowAny;
+from rest_framework.decorators import api_view, permission_classes;
+from rest_framework.permissions import IsAdminUser;
+from .models import Product;
+from .serializers import ProductSerializer;
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger;
 
-# Create your views here.
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])     
+def inventory_list(request):
+    if request.method == 'GET':
+        product_param = request.query_params.get('_id')
+        sort_param = request.query_params.get('sort')
+        product = Product.objects.all()
+        if product_param:
+            product = product.filter(_id=product_param)
+        elif sort_param:
+            product = product.order_by(sort_param)
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name=" Product Name ",
+        price=0,
+        brand="Sample brand ",
+        countInStock=0,
+        category="Sample category",
+        description=" "
+    )
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+# Update single products
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data["name"]
+    product.price = data["price"]
+    product.brand = data["brand"]
+    product.countInStock = data["countInStock"]
+    product.category = data["category"]
+    product.description = data["description"]
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+# Delete a product
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response("Product deleted successfully")
+
+
+# Upload Image
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response("Image was uploaded")
