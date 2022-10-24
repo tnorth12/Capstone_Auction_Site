94b53a460202a62810d8836e8184eb68997e6234
Your Name <you@example.com>
2022-10-23 20:55:37 -0600

    Added Prouct class for fields.

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255) 
+    image = models.ImageField(null=True,blank = True,default = "/images/placeholder.png",upload_to="images/")
+    _id = models.AutoField(primary_key=True,editable=False)
+    brand = models.CharField(max_length=255)     
+    price = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True
+    description = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.name +" | "+self.brand +" | " + str(self.price)
+
+
